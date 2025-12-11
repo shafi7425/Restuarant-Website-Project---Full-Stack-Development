7bb81b8f85bfddeceb7419d81cbd8303d32502e6
shafi7425 <shafiullah.dec1999@gmail.com>
2025-12-12 01:32:29 +0300

    Contact Page Updated

--- frontend/src/pages/frontend/contact-us.js
+++ frontend/src/pages/frontend/contact-us.js
@@ -98,38 +98,7 @@ export function contact() {
                 </div>
             </div>
             <div class="row g-4">
-                <div class="col-xl-8">
-                    <div class="contact-form">
-                        <form class="row g-3">
-                            <div class="col-md-6">
-                                <label for="inputFirstname" class="form-label mt-0">First Name</label>
-                                <input type="text" class="form-control" id="inputFirstname" placeholder="Enter your fist name">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="inputLastname" class="form-label mt-0">Last Name</label>
-                                <input type="text" class="form-control" id="inputLastname" placeholder="Enter your last name">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="inputEmail" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email">
-                            </div>
-                            <div class="col-md-6">
-                                <label for="inputPhone" class="form-label">Phone Number</label>
-                                <input type="tel" class="form-control" id="inputPhone" placeholder="Enter your number">
-                            </div>
-                            <div class="col-md-12">
-                                <label for="inputtext" class="form-label">How Can We Help You?</label>
-                                <textarea class="form-control" id="inputtext" rows="3" placeholder="Hi there, I would like to...."></textarea>
-                            </div>
-                        </form>
-                        <div class="buttons d-flex align-items-center justify-content-end gap-3">
-                            <a href="contact.html.html" class="btn gray-btn mt-0">CANCEL</a>
-                            <a href="index-2.html" class="btn theme-btn mt-0">SUBMIT</a>
-                        </div>
-                    </div>
-                </div>
-               
-                <div class="col-xl-4">
+                <div class="col-xl-12">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126824.48901778966!2d24.66580867767334!3d60.21990912738706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4692099e4e23fa05%3A0xe4fd864d7f1881ee!2sMetropolia%20University%20of%20Applied%20Sciences%20-%20Arabia%20Campus!5e0!3m2!1sen!2ssa!4v1762650084904!5m2!1sen!2ssa"
                         width="600" height="450" class="contact-map border-0 w-100 h-100" allowfullscreen=""
